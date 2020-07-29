bd8a7f29b50390f20e4bbc373d981f6df0155cb4
Robert Fils-Aime <tazaime@gmail.com>
2020-07-28 23:29:04 -0400

    Added Time Rates Last Updated

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const currencyEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
 const amountEl_two = document.getElementById('amount-two');
 
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
+const time = document.getElementById('time');
 
-// Fetch exchange rates and update the DOM
+// Fetch Exchange Rates and Update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
@@ -14,25 +15,55 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      const rate = data.rates[currency_two];
+      const rate = data.rates[currency_two].toFixed(2);
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2); //toFixed for 2 decimal places
+
+      console.log(data);
+
+      // Time Conversion
+      function timeConverter(UNIX_timestamp) {
+        var a = new Date(data.time_last_updated * 1000);
+        var months = [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
+        ];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time =
+          date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+        return time;
+      }
+      console.log(timeConverter(0));
+      time.innerHTML = timeConverter(0);
     });
 }
 
-// Event listeners
+// Event Listeners
 currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
-
 swap.addEventListener('click', () => {
-  const temp = currencyEl_one.value;
+  const tempValue = currencyEl_one.value;
   currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
+  currencyEl_two.value = tempValue;
   calculate();
 });
 
